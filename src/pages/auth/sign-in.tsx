4b78e567dd557ad763bd564825ff34b23219900b
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-05-31 09:36:02 -0300

    feat: add initial sing-in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,62 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    toast.success('A authentication link was sent to your e-mail', {
+      action: {
+        label: 'Re-send',
+        onClick: () => handleSignIn(data),
+      },
+    })
+  }
+
   return (
     <>
       <Helmet title="Login" />
-      <h1>Sign-in</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Access dashboard
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Track your sales through the partner dashboard!
+            </p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Your e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <Button type="submit" disabled={isSubmitting} className="w-full">
+              Access dashboard
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }

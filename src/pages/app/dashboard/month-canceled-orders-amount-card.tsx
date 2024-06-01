0f4ebefba8e82d230cde6f379632132478978b90
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-06-01 08:39:07 -0300

    feat: create initial dashboard page

---
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -0,0 +1,24 @@
+import { Ban } from 'lucide-react'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function MonthCanceledOrdersAmountCard() {
+  return (
+    <Card>
+      <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-base font-semibold">
+          Canceled (month)
+        </CardTitle>
+        <Ban className="size-4 text-muted-foreground" />
+      </CardHeader>
+
+      <CardContent className="space-y-1">
+        <span className="text-2xl font-bold tracking-tight">32</span>
+        <p className="text-xs text-muted-foreground">
+          <span className="text-emerald-500 dark:text-emerald-400">-3%</span> in
+          relation to the past month
+        </p>
+      </CardContent>
+    </Card>
+  )
+}
